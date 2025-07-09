276ea38ecae99aa25e55a388cf251c4c8ca1aca5
lucaskaua <lucaskauadev@gmail.com>
2025-07-09 15:19:49 -0300

    Criando classe Account

---
+++ src/entities/Account.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Account {
+	private final Integer NUMBER;
+	private String name;
+	private Double balance;
+	
+	public Account(Integer number, String name, Double balance) {
+		NUMBER = number;
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	public Integer getNUMBER() {
+		return NUMBER;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	
+}
--- src/program/Main.java
+++ src/program/Main.java
@@ -1,10 +1,20 @@
 package program;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("O que você deseja fazer?");
+		System.out.println("");
+		System.out.println("[1] Criar conta");
+		System.out.println("[2] Acessar conta");
 
+		sc.close();
 	}
 
 }
